a1fdd3de6fadd151977a15f5b5f7b22297f2a047
Howard Chen <cchen277@ucr.edu>
2019-08-08 16:35:46 +0100

    failing a jest test

--- introduction.test.js
+++ introduction.test.js
@@ -3,3 +3,7 @@ const introduction = require("./introduction.js")
 test("introduce hannah", function() {
    expect(introduction("hannah")).toBe("hello my name is hannah"); 
 });
+
+test("introduce nobody", function() {
+    expect(introduction("")).toBe("hello my name is");
+});
